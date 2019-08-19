1ca6f32606900752db30fe8b027e136a295d3aef
Sergey Tovt <st@norse.co>
2019-08-19 16:28:54 +0300

    add basename to Router for gh-pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const Home = React.lazy(() => import('./pages/Home'));
 const About = React.lazy(() => import('./pages/About'));
 
 const App = () => (
-  <Router>
+  <Router basename="/react-parcel-issues">
     <Suspense fallback={<div>Loading...</div>}>
       <Switch>
         <Route exact path="/" component={Home} />
